e7708670e511b3c2fc811b8f12ea4cbb603a65ee
Hung Bui <quanghungbui8104@gmail.com>
2025-10-27 22:07:00 -0500

    fixed import

--- client/src/components/Toaster.tsx
+++ client/src/components/Toaster.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './Toaster.css';
 
 const Toast = ({ id, message, type, duration, onClose } : any) => {
